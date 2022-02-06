13864b8ca1b342c768ed9b5feb9b38d1381a0180
Pierożek <mikolaj.mikolaj@gmail.com>
2022-02-06 12:32:12 +0100

    Remove unused stuff from printer

--- impls/lispreter/printer.py
+++ impls/lispreter/printer.py
@@ -1,4 +1,4 @@
-from mal_types import Symbol, list_type_bracket_map, List, Vector, HashMap
+from mal_types import Symbol, List, Vector, HashMap
 
 
 def pr_str(ast, print_readably=False):
@@ -18,23 +18,3 @@ def pr_str(ast, print_readably=False):
         return '{' + " ".join(k + " " + pr_str(v) for k, v in ast.items()) + '}'
     return str(ast) if not print_readably \
         else str(ast).encode().decode('unicode_escape')
-
-
-def pr_atom(atom, print_readably):
-    if atom is True:
-        return 'true'
-    elif atom is False:
-        return 'false'
-    elif atom is None:
-        return 'nil'
-    elif isinstance(atom, Symbol):
-        return atom.val
-    return str(atom) if not print_readably \
-        else str(atom).encode().decode('unicode_escape')
-
-
-def pr_list(ast):
-    result = list_type_bracket_map[type(ast).__name__].left_bracket
-    for i, x in enumerate(ast):
-        result += pr_str(x) + (' ' if i < len(ast) - 1 else '')
-    return result + list_type_bracket_map[type(ast).__name__].right_bracket
